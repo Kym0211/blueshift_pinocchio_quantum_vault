a135b5bb6433daea7e5599f566c7f8447de2a1c3
Kym0211 <kavyam.ksingh@gmail.com>
2025-08-30 00:02:14 +0530

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+pub mod instructions;
+pub use instructions::*;
+
+use pinocchio::{
+    account_info::AccountInfo, program_entrypoint, program_error::ProgramError,
+    pubkey::Pubkey, ProgramResult
+};
+
+program_entrypoint!(process_instruction);
+
+pub const ID: Pubkey = [
+    0x0f, 0x1e, 0x6b, 0x14, 0x21, 0xc0, 0x4a, 0x07, 
+    0x04, 0x31, 0x26, 0x5c, 0x19, 0xc5, 0xbb, 0xee, 
+    0x19, 0x92, 0xba, 0xe8, 0xaf, 0xd1, 0xcd, 0x07, 
+    0x8e, 0xf8, 0xaf, 0x70, 0x47, 0xdc, 0x11, 0xf7, 
+];
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8] 
+) -> ProgramResult {
+
+    match instruction_data.split_first() {
+        _ => Err(ProgramError::InvalidInstructionData)
+    }
+}
